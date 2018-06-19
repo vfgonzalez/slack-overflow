89ae59e73de185e95ef591537657d360d3bf2d26
Jeremy Knappe <jdknappe@me.com>
2018-06-18 20:41:35 -0700

    results card with upvote/downvote buttons

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -2,8 +2,28 @@ import React from "react";
 import "./Results.css";
 
 const Results = props => (
-  <div className="col-lg-12">
-    <p>Results</p>
+  <div className="row">
+    <div className="col s12 m3">
+      <div className="card-panel teal">
+        <div className="card-content">
+          <div className="card-title">
+          Resource title
+          </div>
+          <div className="card-image">
+          Category image
+          </div>
+          <div className="card-content">
+          Resource summary
+          </div> 
+          <div>
+            <button waves="light">Upvote</button>
+          </div>
+          <div>
+            <button waves="light">Downvote</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
