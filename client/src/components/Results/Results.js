d4cabea18078ce35d7bc697fc324e0da4c78e716
Jeremy Knappe <jdknappe@me.com>
2018-06-18 21:06:31 -0700

    resource card is improved with prettier buttons, external link button, background image placeholder and card title

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -2,30 +2,19 @@ import React from "react";
 import "./Results.css";
 import { Button } from "react-materialize";
 import { Icon } from "react-materialize";
+import { Card } from "react-materialize";
+import { CardTitle } from "react-materialize";
 const Results = props => (
-  <div className="row">
-    <div className="col s12 m3">
-      <div className="card-panel teal">
-        <div className="card-content">
-          <div className="card-title">
-          Resource title
-          </div>
-          <div className="card-image">
-          Category image
-          </div>
-          <div className="card-content">
-          Resource summary
-          </div> 
-          <div>
-            <Button waves='light'>Upvote<Icon left>arrow_upward</Icon></Button>
-          </div>
-          <div>
-            <Button waves='light'>Downvote<Icon right>arrow_downward</Icon></Button>
-          </div>
-        </div>
-      </div>
+  <Card className='small'
+    header={<CardTitle image="http://via.placeholder.com/140x100">Resource Title</CardTitle>}
+    actions={[<a href='#'>External Link</a>]}>
+    Resource summary.
+    <div>
+      <Button waves='light'>Upvote<Icon left>arrow_upward</Icon></Button>
+  
+      <Button waves='light'>Downvote<Icon right>arrow_downward</Icon></Button>
     </div>
-  </div>
+  </Card>
 );
 
 export default Results;
