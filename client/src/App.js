c8a7a0dba2299dd7abf2407c37b8323eb8ad58d4
Christian Lopes <christianlopes@ChrisMacBook-Pro.local>
2018-06-19 20:35:05 -0700

    carousel version 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
-import logo from "./logo.svg";
-import MenuAppBar from "./components/Nav/Nav";
-import Categories from "./components/Categories/Categories";
+// import ReactDOM from 'react-dom';
+import Nav from "./components/Nav/Nav";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import Post from "./components/Post/Post";
 import Results from "./components/Results/Results";
 import Footer from "./components/Footer/Footer";
+// import cards from "./cards.json";
+import Categories from "./components/Categories/Categories";
+
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MenuAppBar />
+        <Nav />
         <Post />
         <Jumbotron />
-        <Categories />
+        <Categories/>
         <Results />
         <Footer />
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
       </div>
     );
   }
--- client/src/components/Categories/Categories.js
+++ client/src/components/Categories/Categories.js
@@ -1,10 +1,48 @@
 import React from "react";
 import "./Categories.css";
+import { Carousel} from "react-materialize";
+// import cards from "../cards.json";
 
 const Categories = props => (
-  <div className="col-lg-12">
-    <p>Categories</p>
-  </div>
+
+    <div>
+
+        <Carousel images={[
+
+            <a href="">
+                <img src="./images/generaltools.png" />
+            </a>,
+
+            <a href="">
+                <img src="./images/teachingresources.png" />
+            </a>,
+
+            <a href="">
+                <img src="./images/css.png" />
+            </a>,
+
+            <a href="">
+                <img src="./images/testing.png" />
+            </a>,
+
+            <a href="">
+                <img src="./images/javascript.png" />
+            </a>,
+
+            <a href="">
+                <img src="./images/api.png" />
+            </a>,
+
+            <a href="">
+                <img src="./images/databases.png" />
+            </a>
+
+        ]} />
+
+        <p> hello world</p>
+
+
+    </div>
 );
 
 export default Categories;
