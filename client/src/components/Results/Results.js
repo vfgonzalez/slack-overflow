e696053e498ac1e78fbe7fdcb4b62fff165b9d73
Jeremy Knappe <jdknappe@me.com>
2018-06-20 21:27:26 -0700

    added 3 columns to a row. Next task is to write logic to populate the rows with info from our database.

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -7,21 +7,49 @@ import { CardTitle } from "react-materialize";
 import { Row } from "react-materialize";
 import { Col } from "react-materialize";
 const Results = props => (
-  <Row>
-    <Col s={4}>
-      <Card header={<CardTitle reveal image={"http://via.placeholder.com/350x150"} waves='light' />}
-        title="Impressive Resource Title"
-        reveal={<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-          dolore eu fugiat nulla pariatur.</p>}>
-        <p><a href="#">http://www.linkgoeshere.com/</a></p>
-        <Button className="btn-left" waves='light'>Upvote<Icon left>arrow_upward</Icon></Button>
-        <Button className="btn-right" waves='light'>Downvote<Icon right>arrow_downward</Icon></Button>
-      </Card>
-    </Col>
-  </Row>
+  <div className="container">
+    <Row>
+      <Col s={4}>
+        <Card className="sticky-action" header={<CardTitle reveal image={"http://via.placeholder.com/350x150"} waves='light' />}
+          title="Impressive Resource Title"
+          reveal={<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+            dolore eu fugiat nulla pariatur.</p>}>
+          <p><a href="#">http://www.linkgoeshere.com/</a></p>
+          <Button className="btn-left green" waves='light'><Icon center>arrow_upward</Icon></Button>
+          <Button className="btn-right red" waves='light'><Icon center>arrow_downward</Icon></Button>
+        </Card>
+      </Col>
+      <Col s={4}>
+        <Card className="sticky-action card" header={<CardTitle reveal image={"http://via.placeholder.com/350x150"} waves='light' />}
+          title="Impressive Resource Title"
+          reveal={<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur.</p>}>
+          <p><a href="#">http://www.linkgoeshere.com/</a></p>
+          <Button className="btn-left green" waves='light'><Icon center>arrow_upward</Icon></Button>
+          <Button className="btn-right red" waves='light'><Icon center>arrow_downward</Icon></Button>
+        </Card>
+      </Col>
+      <Col s={4}>
+        <Card className="sticky-action" header={<CardTitle reveal image={"http://via.placeholder.com/350x150"} waves='light' />}
+          title="Impressive Resource Title"
+          reveal={<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur.</p>}>
+          <p><a href="#">http://www.linkgoeshere.com/</a></p>
+          <Button className="btn-left green" waves='light'><Icon center>arrow_upward</Icon></Button>
+          <Button className="btn-right red" waves='light'><Icon center>arrow_downward</Icon></Button>
+        </Card>
+      </Col>
+    </Row>
+  </div>
 );
 
 export default Results;
