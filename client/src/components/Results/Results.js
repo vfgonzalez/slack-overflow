9e159f6e354bc6516d247f383823a49064a17f6f
Jeremy Knappe <jdknappe@me.com>
2018-06-20 20:00:20 -0700

    minor fix to button issue

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -10,15 +10,15 @@ const Results = props => (
   <Row>
     <Col s={4}>
       <Card header={<CardTitle reveal image={"http://via.placeholder.com/350x150"} waves='light' />}
-        title="Card Title"
+        title="Impressive Resource Title"
         reveal={<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
           veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
           dolore eu fugiat nulla pariatur.</p>}>
-        <p><a href="#">External link</a></p>
-        <Button waves='light'>Upvote<Icon left>arrow_upward</Icon></Button>
-        <Button className="btn-right-align" waves='light'>Downvote<Icon right>arrow_downward</Icon></Button>
+        <p><a href="#">http://www.linkgoeshere.com/</a></p>
+        <Button className="btn-left" waves='light'>Upvote<Icon left>arrow_upward</Icon></Button>
+        <Button className="btn-right" waves='light'>Downvote<Icon right>arrow_downward</Icon></Button>
       </Card>
     </Col>
   </Row>
