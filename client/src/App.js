ae316e55dc7d7b5af618c0607e307b9b62429b39
Christian Lopes <christianlopes@dhcp-v074-148.mobile.uci.edu>
2018-06-23 10:04:59 -0700

    carousel with onClick

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,42 @@ import Footer from "./components/Footer/Footer";
 import Categories from "./components/Categories/Categories";
 
 
+
 import "./App.css";
 
 class App extends Component {
+
+  state = {
+    categoryName: '',
+    results: [],
+  };
+
+  handleImageClick = (category) => {
+    console.log('category', category);
+    this.setState({ categoryName: category.name });
+
+    // TODO: use category.id to make an api call & get results
+    // axios.get('/api/things/' + category.id, (results) => {
+    //   console.log('results', results);
+    //   this.setState({ results });
+    // });
+  }
+
   render() {
     return (
       <div className="App">
         <Nav />
         <Post />
         <Jumbotron />
-        <Categories/>
-        <Results />
+        <Categories
+          onImageClick={this.handleImageClick}
+        />
+        <div className="border row">
+          <div className="category col s12">{this.state.categoryName}</div>
+        </div>
+        <Results
+          results={this.state.results}
+        />
         <Footer />
       </div>
     );
--- client/src/components/Categories/Categories.js
+++ client/src/components/Categories/Categories.js
@@ -1,122 +1,34 @@
 import React from "react";
 import "./Categories.css";
 import { Carousel } from "react-materialize";
-// import catArr from "./catArray.json";
-
-let catArray = [
-
-    {
-        "id": 1,
-        "name": "General Tools",
-        "image": "./images/generaltools.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 2,
-        "name": "Teaching Resources",
-        "image": "./images/teachingresources.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 3,
-        "name": "CSS",
-        "image": "./images/css.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 4,
-        "name": "Testing",
-        "image": "./images/testing.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 5,
-        "name": "Javascript",
-        "image": "./images/javascript.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 6,
-        "name": "API",
-        "image": "./images/api.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 7,
-        "name": "Databases",
-        "image": "./images/databases.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 8,
-        "name": "NPM Packages",
-        "image": "./images/npmpackages.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 9,
-        "name": "Templates",
-        "image": "./images/templates.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 10,
-        "name": "Common Issues",
-        "image": "./images/commonissues.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 11,
-        "name": "Job Resources",
-        "image": "./images/jobresources.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 12,
-        "name": "HTML",
-        "image": "./images/html.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 13,
-        "name": "Random",
-        "image": "./images/random.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 14,
-        "name": "Podcasts",
-        "image": "./images/podcasts.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 15,
-        "name": "Slack Submissions",
-        "image": "./images/slacksubmissions.png",
-        "description": "Enter text here"
-    },
-    {
-        "id": 16,
-        "name": "Text Submissions",
-        "image": "./images/textsubmissions.png",
-        "description": "Enter text here"
-    }
-]
+import catArray from "../../catArray.json";
 
 const Categories = props => (
 
     <div>
-        <Carousel options={{ dist: -60, shift: 60, indicators: true }} images={[
 
-            catArray.map((cat) => {
-                return (
-                    <a href="">
-                        <img src={cat.image} />
-                    </a>
-                );
-            })
-        ]} />
+        <div>
+            <Carousel
+                options={{
+                    dist: -60,
+                    shift: 60,
+                    indicators: true
+                }}
+                children={catArray.map((cat) => {
+                    return (
+                        <img
+                            key={cat.id}
+                            onClick={() => { props.onImageClick(cat) }}
+                            src={cat.image}
+                        />
+                    );
+                })}
+            />
+
+        </div>
+
     </div>
+
 );
 
 export default Categories;
