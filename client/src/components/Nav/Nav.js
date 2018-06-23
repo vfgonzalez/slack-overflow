6eeafa76a9d087ebb229a256a607e2355b767ac7
Jeremy Knappe <jdknappe@me.com>
2018-06-23 11:07:06 -0700

    added Login.css file and played with buttons styling in nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -9,16 +9,16 @@ import Sidebar from '../Sidebar/Sidebar'
 class Nav extends Component {
   render() {
     return (
-      <Navbar brand={<Sidebar/>} right
-      className="grey darken-2 left-align"
-      >
-     
+    <Navbar brand={<Sidebar/>} right
+      className="grey darken-2 left-align">
+    
       <NavItem >
         
         <Login/>
+
       </NavItem>
 
-      
+  
     </Navbar>
     );
   }
