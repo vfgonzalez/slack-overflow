b1151675e26b16036627c4094bc9e10ed0e8239f
Vicente Gonzalez <vgonzalez28@gmail.com>
2018-06-23 23:32:38 -0700

    utils file update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,5 +7,22 @@ export default {
     return axios.get("/api/resources", { params: { q: query } });
   console.log(query);
   
-  }
-};
+  },
+  // Gets all resources
+ getResources: function() {
+     return axios.get("/api/resources")
+   },
+   // Gets the resource with the given id
+   getResource: function(id) {
+     return axios.get("/api/resources/" +id);
+   },
+   // Deletes the resource with the given id
+    deleteResource: function(id) {
+     return axios.delete("/api/resources/"+ id);
+   },
+   // Saves a resource to the database
+   saveResource: function(resourceData) {
+     return axios.post("/api/resources", resourceData);
+   }
+  }; 
+
