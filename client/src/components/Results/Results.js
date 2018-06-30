6351ce2700fff014526c09534c212aef81e0a3bb
Christian Lopes <christianlopes@ChrisMacBook-Pro.local>
2018-06-29 18:27:44 -0700

    6.29 synch

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import "./Results.css";
-import { Button } from "react-materialize";
-import { Icon } from "react-materialize";
-import { Card } from "react-materialize";
-import { CardTitle } from "react-materialize";
-import { Row } from "react-materialize";
-import { Col } from "react-materialize";
-
-
+import { Button, Icon, Card, CardTitle } from "react-materialize";
 
 class Results extends Component {
 
@@ -24,17 +17,17 @@ class Results extends Component {
   }
 
   componentDidMount() {
-    const { title, category, createAt, createAd,description, link, voteCount, _id, author } = this.props.children
-  
+    const { title, category, createAt, createAd, description, link, voteCount, _id, author } = this.props.children
+
     this.setState({
       //you can also do it like this - title: title,
-      
+
       title,
       category,
       createAt,
       createAd,
       description,
-      link, 
+      link,
       voteCount,
       _id,
       author
@@ -45,22 +38,21 @@ class Results extends Component {
   render() {
     return (
       <div className="container">
-        <Row>
-          <Col s={3}>
-            <Card className="sticky-action card" header={<CardTitle reveal image={"http://via.placeholder.com/350x150"} waves='light' />}
-
+        <div className="row">
+          <div className="col s4">
+            <Card header={<CardTitle reveal image={"http://via.placeholder.com/350x150"} waves='light' />}
               title={this.state.title}
               reveal={<p>{this.state.description}</p>}>
-              <Button><p><a rel="noreferrer noopener" target="_blank" href={this.state.link}>View Article</a></p></Button>
+              <Button><p><a className="link" rel="noreferrer noopener" target="_blank" href={this.state.link}>View Article</a></p></Button>
               <Button id="upvote" className="btn-upvote green" waves='light'><Icon center>thumb_up</Icon></Button>
               <Button id="downvote" className="btn-downvote red" waves='light'><Icon center>thumb_down</Icon></Button>
               <div id="voteCount"><p>{this.state.voteCount}</p></div>
               <div id="author"><p>{this.state.author}</p></div>
               <div id="dateCreated"><p>{this.state.createAd}</p></div>
             </Card>
-          </Col>
-        </Row>
-      </div>
+          </div>
+        </div >
+      </div >
     );
   }
 }
